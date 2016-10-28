095d040b86ad906c92420e871b6d5427099d8105
Edward Park <ed.sh.park@gmail.com>
2016-10-28 13:54:11 -0700

    React shell for Splash, Login, Signup, Feed, and Tile

--- client/main.js
+++ client/main.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, browserHistory, hashHistory, IndexRoute, IndexLink } from 'react-router'
 
 
 ///////////////////////////////////
 // Imported React components
-import App from './components/App';
+import Start from './components/Start';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Feed from './components/Feed';
+import Browse from './components/Browse';
+import Requested from './components/Requested';
 
-render(
-  <App />,
-  document.getElementById('app')
-)
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={Start}>
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+    </Route>
+    <Route path="/feed" component={Feed}>
+      <Route path="/browse" component={Browse} />
+      <Route path="/requested" component={Requested} />
+    </Route>
+  </Router>
+), document.getElementById('app'))
