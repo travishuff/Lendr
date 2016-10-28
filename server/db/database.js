a291f0d54a8cba5e788cf2d8c816c3237b3c16cd
Andres Olivero <andres.eolivero@gmail.com>
2016-10-28 14:03:10 -0700

    took out unnecessary comments

--- server/db/database.js
+++ server/db/database.js
@@ -1,17 +1,3 @@
-// const pg = require('pg');
-
-// const db = {};
-
-// ///////////////////////////////////
-// // Figure out where to direct PG database
-// // Defaults to your OSX login and username
-// const uri;
-
-// pg.connect(uri, (err, db_) => {
-//   if (err) throw new Error(err);
-//   db.conn = db_;
-// });
-
 const Sequelize = require('sequelize');
 const itemController = require('/item_controller');
 const userController = require('/user_controller');
