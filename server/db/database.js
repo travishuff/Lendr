d72b3be15440ba8fa22cac488e69e3a17882339d
Andres Olivero <andres.eolivero@gmail.com>
2016-10-28 19:02:50 -0700

    updates to server logic

--- server/db/database.js
+++ server/db/database.js
@@ -3,11 +3,13 @@ const itemController = require('/item_controller');
 const userController = require('/user_controller');
 const requestController = require('/request_controller');
 
-// setting up a sequel connection pool using postgres as the dialect
+// setting up a sequel connection pool using postgres as the dialect 
 const sequelize = new Sequelize('database.js', 'AndresOlivero', 'password', {
+  //what gets added as username and password??
   host: 'localhost',
   dialect: 'postgres'
 });
+// authenticating that the sequel connection was successfully created
 sequelize
   .authenticate()
   .then(function(err) {
@@ -17,39 +19,29 @@ sequelize
     console.log('Unable to connect to the database:', err);
   });
 
-// defining models for all three needed tables: Items, Users, and Requests,
-// pulling the schemas from the controller file
-let Item = sequelize.define('item', itemController.itemSchema);
-let User = sequelize.define('user', userController.userSchema);
-let Request = sequelize.define('request', requestContoller.requestSchema);
-
-
-// exporting the function that will sync whichever model was input from the user. 
+// exporting the function that perform actions based on the input request data
 module.exports = function (data) {
 
-  let item = {};
-  let user = {};
-  let request = {};
-
   for (let action in data) {
-    if (action === 'item') {
-      item = data[action];
-      sequelize.sync({ logging: console.log }).then(() => {
-        Item.create(item, { validate: true }).catch(function (errors) { console.log('error:', errors) });
-      });
-
+    if (action === 'createItem') {
+      // data[action] should be equal to obj with all props that an item takes
+      itemController.createItem(data[action]);
     }
-    if (action === 'user') {
-      user = data[action];
-      sequelize.sync({ logging: console.log }).then(() => {
-        User.create(user, { validate: true }).catch(function (errors) { console.log('error:', errors) });
-      });
+    if (action === 'createUser') {
+      // data[action] should be equal to obj with all props that a user takes
+      userController.createUser(data[action]);
     }
-    if (action === 'request') {
-      request = data[action];
-      sequelize.sync({ logging: console.log }).then(() => {
-        Request.create(request, { validate: true }).catch(function (errors) { console.log('error:', errors) });
-      });
+    if (action === 'createRequest') {
+      // data[action] should be equal to obj with all props that a request takes
+      requestController.createRequest(data[action]);
+    }
+    if (action === 'verifyUser') {
+      // data[action] should be equal to a string that equals a user's username or userId??
+      userController.verifyUser(data[action]);
+    }
+    if (action === 'getWishlist') {
+      // data[action] should be equal to a string that equals a user's username or userId??
+      requestController.getWishlist(data[action]);
     }
   }
 }
